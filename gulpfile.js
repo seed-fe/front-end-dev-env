c5ad059fa2623b05154193363f1a52927a1f22d1
Bill Zhang <zzlfullstack@163.com>
2019-07-15 17:53:31 +0800

    修改了gulpfile.js的一些注释和配置

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ const handleError = function(err) {
 /*开发阶段，不用gulp-clean-css进行压缩*/
 /*配置SASS任务*/
 gulp.task('watchsass', function() {
-    /*监听LESS文件，并在修改后自动编译成CSS，然后通知浏览器并注入CSS*/
+    /*监听sass文件，并在修改后自动编译成CSS，然后通知浏览器并注入CSS*/
     gulp.watch('src/sass/**/*.scss', function (event) {
         var paths = plugins.watchPath(event, 'src/sass/', 'src/css/');
         // 文件操作的事件类型
@@ -27,9 +27,9 @@ gulp.task('watchsass', function() {
         gutil.log('Dist ' + paths.distPath);
         // 用stream-combiner2合并多个stream，只需添加一个错误监听，而且不会让gulp停止运行
         var combined = combiner.obj([
-            // 从src/less/取得LESS文件
+            // 从src/sass/取得SASS文件
             gulp.src(paths.srcPath),
-            // 这里开启sourcemap后会生成sourcemap文件，可以在firefox（要在开发者工具的设置里样式编辑器部分勾选显示原始来源）和chrome（同样要在开发者工具的设置里勾选enable JavaScript sourcemaps和enable css sourcemaps）自带的开发者工具里直接调试LESS了
+            // 这里开启sourcemap后会生成sourcemap文件，可以在firefox（要在开发者工具的设置里样式编辑器部分勾选显示原始来源）和chrome（同样要在开发者工具的设置里勾选enable JavaScript sourcemaps和enable css sourcemaps）自带的开发者工具里直接调试SASS了
             plugins.sourcemaps.init(),
             // 编译SASS，outputStyle选项表示输出CSS的风格，默认是nested，对sass里的嵌套在编译成CSS后也会缩进，expanded会在输出CSS的每一个选择器设置之间空一行，compact也在每个选择器之间空一行，但是每个选择器的样式设置都只写在一行里—（就是花括号里的所有样式设置都写在一行），compressed输出压缩后的CSS
             plugins.sass({outputStyle: 'expanded'}),
@@ -45,7 +45,7 @@ gulp.task('watchsass', function() {
         return combined;
     })
 });
-// 先执行监听和编译LESS的任务，然后启动Browsersync，并监听src/路径下所有html、css和js文件（js文件还需要js-hint等前置的任务，可以防止watchjs任务里）
+// 先执行监听和编译SASS的任务，然后启动Browsersync，并监听src/路径下所有html、css和js文件（js文件还需要js-hint等前置的任务，可以放在watchjs任务里）
 gulp.task('reload',['watchsass'], function() {
     return browserSync.init({
         // 设置监听的文件，以baseDir设置的根目录为起点，单个文件就用字符串，多个文件就用数组
@@ -59,10 +59,10 @@ gulp.task('reload',['watchsass'], function() {
             clicks: true,
             scroll: true
         },
-        // 更改控制台日志前缀
+        // 更改控制台日志前缀，可以用项目名，每个项目单独修改即可
         logPrefix: "sass & requirejs with gulp",
-        // 设置监听时打开的浏览器
-        browser: "firefox",
+        // 设置监听时默认打开的浏览器
+        browser: "chrome",
         // 设置服务器监听的端口号
         port: 8080
     });
